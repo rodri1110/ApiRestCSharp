28b1ef3e9d42b9ffc538b7619994d6690796e16c
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-04 02:00:29 -0300

    add razor import and department view created

---
+++ SalesApiCSharp/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesApiCSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesApiCSharp.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department{Id = 1, Name = "Eletronics"});
+            list.Add(new Department{Id = 2, Name = "Books"});
+            return View(list);
+        }
+    }
+}
