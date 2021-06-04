eaa698594c6f1f0966d3fd005cfbc7188445f406
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-04 08:38:26 -0300

    CRUD Scaffolding

---
+++ SalesApiCSharp/Data/SalesApiCSharpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesApiCSharp.Models;
+
+namespace SalesApiCSharp.Data
+{
+    public class SalesApiCSharpContext : DbContext
+    {
+        public SalesApiCSharpContext (DbContextOptions<SalesApiCSharpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesApiCSharp.Models.Department> Department { get; set; }
+    }
+}
--- SalesApiCSharp/Startup.cs
+++ SalesApiCSharp/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesApiCSharp.Data;
 
 namespace SalesApiCSharp
 {
@@ -33,6 +35,9 @@ namespace SalesApiCSharp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesApiCSharpContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesApiCSharpContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
