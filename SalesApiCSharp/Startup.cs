22c9fdb7e7dd893f24203f42aded6999546df3c1
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-07 10:43:39 -0300

    First Migration and adjust configutration

--- SalesApiCSharp/Startup.cs
+++ SalesApiCSharp/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesApiCSharp
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesApiCSharpContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesApiCSharpContext")));
+                options.UseMySql(Configuration.GetConnectionString("SalesApiCSharpContext"), builder =>
+                    builder.MigrationsAssembly("SalesApiCSharp")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
