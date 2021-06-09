e8bad47077abd88c3d85925cb006020e7dfddd5a
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-09 13:18:00 -0300

    Seller Service and basic FindAll

---
+++ SalesApiCSharp/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using SalesApiCSharp.Data;
+using SalesApiCSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesApiCSharp.Services
+{
+    public class SellerService
+    {
+        private readonly SalesApiCSharpContext _context;
+
+        public SellerService(SalesApiCSharpContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesApiCSharp/Startup.cs
+++ SalesApiCSharp/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesApiCSharp.Data;
+using SalesApiCSharp.Services;
 
 namespace SalesApiCSharp
 {
@@ -41,6 +42,7 @@ namespace SalesApiCSharp
                     builder.MigrationsAssembly("SalesApiCSharp")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
