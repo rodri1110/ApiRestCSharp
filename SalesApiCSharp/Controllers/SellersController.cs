cdf40f263b107515feb2f666861b16cb4c6cc94b
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-15 23:29:49 -0300

    Update Seller and custom service exception

--- SalesApiCSharp/Controllers/SellersController.cs
+++ SalesApiCSharp/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesApiCSharp.Models;
 using SalesApiCSharp.Models.ViewModels;
 using SalesApiCSharp.Services;
+using SalesApiCSharp.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -52,7 +53,7 @@ namespace SalesApiCSharp.Controllers
             {
                 return NotFound();
             }
-            return View(obj);            
+            return View(obj);
         }
 
         [HttpPost]
@@ -75,6 +76,45 @@ namespace SalesApiCSharp.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- SalesApiCSharp/Services/SellerService.cs
+++ SalesApiCSharp/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesApiCSharp.Services.Exceptions;
 
 namespace SalesApiCSharp.Services
 {
@@ -29,7 +30,7 @@ namespace SalesApiCSharp.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);            
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
@@ -38,6 +39,21 @@ namespace SalesApiCSharp.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
-
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found!");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
     }
 }
