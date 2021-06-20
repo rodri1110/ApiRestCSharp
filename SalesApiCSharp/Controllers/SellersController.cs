608730f390529fa4821272f071d799bc26eaef59
rodri1110 <rodri.oliveira1110@gmail.com>
2021-06-20 20:09:44 -0300

    validation

--- SalesApiCSharp/Controllers/SellersController.cs
+++ SalesApiCSharp/Controllers/SellersController.cs
@@ -39,6 +39,12 @@ namespace SalesApiCSharp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Inserir(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -99,6 +105,12 @@ namespace SalesApiCSharp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return BadRequest();
